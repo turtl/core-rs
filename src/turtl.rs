ee7b3d4d64e3be81aa379bc92ca285fd6c1dbc00
Andrew Danger Lyon <orthecreedence@gmail.com>
2016-07-19 15:25:29 -0700

    beginnings of turtl module for holding app state

---
+++ src/turtl.rs
@@ -0,0 +1,22 @@
+//! The Turtl module is the container for the state of the app. It provides
+//! functions/interfaces for updating or retrieving stateful info about the app.
+
+use ::util::event;
+use ::models::user::User;
+use std::sync::RwLock;
+
+/// Defines a container for our app's state
+struct Turtl<'event> {
+    pub events: event::EventEmitter<'event>,
+    pub user: User<'event>
+}
+
+/*
+lazy_static! {
+    static ref TURTL: RwLock<Turtl<'static>> = RwLock::new(Turtl {
+        events: event::EventEmitter::new(),
+        user: User::blank(),
+    });
+}
+*/
+
