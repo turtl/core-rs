d8fe4cb162a7846408d479ad5bb2f0594a5d5060
Andrew Danger Lyon <orthecreedence@gmail.com>
2016-10-07 20:20:48 -0700

    adding client crate...this is a mock turtl client that talks to the app via the messaging system. this is necessary because i no longer can use nanocat as a means to test the core, so having a client that "speaks the language" is going to be very useful for high-level testing. if there were a way to say run aLL test but THIS one, i would just have this stuff live in turtl core itself, however client might be a nice way to eventually transition to a turtl CLI or something

---
+++ client/src/main.rs
@@ -0,0 +1,81 @@
+extern crate carrier;
+extern crate config;
+extern crate turtl_core;
+
+use ::std::env;
+use ::std::thread;
+use ::std::time::Duration;
+
+pub fn sleep(millis: u64) {
+    thread::sleep(Duration::from_millis(millis));
+}
+
+pub fn init() -> thread::JoinHandle<()> {
+    if env::var("TURTL_CONFIG_FILE").is_err() {
+        env::set_var("TURTL_CONFIG_FILE", "../config.yaml");
+    }
+
+    carrier::wipe();
+
+    thread::spawn(|| {
+        turtl_core::init().unwrap();
+        let app_config = String::from(r#"{"data_folder":"d:/tmp/turtl/"}"#);
+        turtl_core::start(app_config).join().unwrap();
+    })
+}
+
+pub fn send(msg: &str) {
+    let channel: String = config::get(&["messaging", "address"]).unwrap();
+    carrier::send_string(&format!("{}-core-in", channel), String::from(&msg[..])).unwrap();
+}
+
+pub fn recv() -> String {
+    let channel: String = config::get(&["messaging", "address"]).unwrap();
+    String::from_utf8(carrier::recv(&format!("{}-core-out", channel)).unwrap()).unwrap()
+}
+
+fn main() {
+    let handle = init();
+
+    send(r#"["ping"]"#);
+    let msg = recv();
+    println!("client: got {}", msg);
+
+    handle.join().unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use ::std::thread;
+
+    use ::carrier;
+    use ::config;
+
+    use super::*;
+
+    fn end(handle: thread::JoinHandle<()>) {
+        handle.join().unwrap();
+        carrier::wipe();
+    }
+
+    #[test]
+    fn inits_shuts_down() {
+        let handle = init();
+        send(r#"["app:shutdown"]"#);
+        end(handle);
+    }
+
+    #[test]
+    fn login() {
+        let handle = init();
+        let username: String = config::get(&["client", "test", "username"]).unwrap();
+        let password: String = config::get(&["client", "test", "password"]).unwrap();
+        let msg = format!(r#"["user:login",{{"username":"{}","password":"{}"}}]"#, username, password);
+        println!("msg: {}", msg);
+        send(&msg[..]);
+        let msg = recv();
+        println!("login: recv: {}", msg);
+        end(handle);
+    }
+}
+
