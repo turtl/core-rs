6cf7dda84f8e7a4bc29feccdc3dea302acba37f2
Andrew Danger Lyon <orthecreedence@gmail.com>
2016-11-03 09:52:30 -0700

    file model: adding FileData type, commenting

--- src/models/file.rs
+++ src/models/file.rs
@@ -4,12 +4,25 @@ use ::models::model::Model;
 use ::models::protected::Protected;
 
 protected!{
+    /// Defines the object we find inside of Note.File (a description of the
+    /// note's file with no actual file data...name, mime type, etc).
     pub struct File {
         ( size: u64,
           has_data: bool ),
         ( name: String,
           type_: String,
           meta: Value ),
+        ( )
+    }
+}
+
+protected!{
+    /// Defines the object that holds actual file body data separately from the
+    /// metadata that lives in the Note object.
+    pub struct FileData {
+        ( note_id: String,
+          has_data: bool ),
+        ( data: Vec<u8> ),
         ( )
     }
 }
