b7aefe531133a5db124fb117b107684b2c34da09
Andrew Danger Lyon <orthecreedence@gmail.com>
2017-08-16 21:21:06 -0700

    test: convert set_api_endpoint

--- tests/set_api_endpoint.rs
+++ tests/set_api_endpoint.rs
@@ -7,9 +7,7 @@ mod tests {
     #[test]
     fn set_api_endpoint() {
         let handle = init();
-        send(r#"["1","app:api:set-endpoint","http://api.turtl.dev:8181"]"#);
-        let msg = recv("1");
-        assert_eq!(msg, r#"{"e":0,"d":{}}"#);
+        dispatch_ass(json!(["app:api:set-endpoint", "http://api.turtl.dev:8181"]));
         end(handle);
     }
 }
