26f8c59b1bf02cd51f01d681a59ad53d0ea9c9a2
Andrew Danger Lyon <orthecreedence@gmail.com>
2017-11-20 23:22:45 -0800

    sock server: make sure we call turtl::init() (for logging) and updating the logging a bit

--- sock/src/main.rs
+++ sock/src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate log;
 extern crate turtl_core;
 extern crate websocket;
 
@@ -16,29 +18,32 @@ pub fn main() {
     if env::var("TURTL_CONFIG_FILE").is_err() {
         env::set_var("TURTL_CONFIG_FILE", "../config.yaml");
     }
-    let handle = turtl_core::start(String::from(r#"{"loglevel":"debug","messaging":{"reqres_append_mid":false}}"#));
+    turtl_core::init().unwrap();
+    let handle = turtl_core::start(String::from(r#"{"messaging":{"reqres_append_mid":false}}"#));
     let server = Server::bind("127.0.0.1:7472").unwrap();
-    println!("- sock server bound, listening");
+    info!("* sock server bound, listening");
     for connection in server.filter_map(Result::ok) {
         thread::spawn(move || {
-            println!("- new connection!");
+            info!("* new connection!");
             let mut client = connection.accept().unwrap();
             client.set_nonblocking(true).unwrap();
             loop {
                 let msg_res = client.recv_message();
                 match msg_res {
                     Ok(msg) => {
-                        println!("- got msg from ui");
                         match msg {
                             OwnedMessage::Close(_) => { break; }
                             OwnedMessage::Binary(x) => {
+                                info!("* ui -> core ({})", x.len());
                                 let msg_str = String::from_utf8(x).unwrap();
                                 turtl_core::send(msg_str).unwrap();
                             }
-                            OwnedMessage::Text(x) => { turtl_core::send(x).unwrap(); }
+                            OwnedMessage::Text(x) => {
+                                info!("* ui -> core ({})", x.len());
+                                turtl_core::send(x).unwrap();
+                            }
                             _ => {}
                         }
-                        println!("- send ui msg to dispatch");
                     }
                     Err(_) => {
                     }
@@ -47,7 +52,7 @@ pub fn main() {
                 let msg_turtl = turtl_core::recv_nb(None).unwrap();
                 match msg_turtl {
                     Some(x) => {
-                        println!("- got core message, sending to ui {}", x.len());
+                        info!("* core -> ui ({})", x.len());
                         client.send_message(&Message::text(x)).unwrap();
                     }
                     None => {}
@@ -56,7 +61,7 @@ pub fn main() {
                 let msg_turtl = turtl_core::recv_event_nb().unwrap();
                 match msg_turtl {
                     Some(x) => {
-                        println!("- got core message, sending to ui {}", x.len());
+                        info!("* core -> ui ({})", x.len());
                         client.send_message(&Message::text(x)).unwrap();
                     }
                     None => {}
