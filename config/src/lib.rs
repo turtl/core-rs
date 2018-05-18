8bfca29df68ef82264dfcbedef542ba896875480
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-05-17 23:25:00 -0700

    OOPS FORGOT THIS TWO COMMITS AGO LOOL

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -16,28 +16,22 @@ pub type TResult<T> = Result<T, JSONError>;
 
 lazy_static! {
     /// create a static/global CONFIG var, and load it with our config data
-    static ref CONFIG: RwLock<Value> = {
-        match load_config() {
-            Ok(x) => RwLock::new(x),
-            Err(e) => {
-                panic!("error loading config: {}", e);
-            },
-        }
-    };
+    static ref CONFIG: RwLock<Value> = RwLock::new(Value::Null);
 }
 
 /// load/parse our config file, and return the parsed JSON value
-fn load_config() -> TResult<Value> {
-    let path_env = match env::var("TURTL_CONFIG_FILE") {
-        Ok(x) => x,
-        Err(_) => String::from("config.yaml"),
-    };
+pub fn load_config(location: Option<String>) -> TResult<()> {
+    let path_env = location
+        .unwrap_or(env::var("TURTL_CONFIG_FILE").unwrap_or(String::from("config.yaml")));
     let path = Path::new(&path_env[..]);
     let mut file = File::open(&path)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let data: Value = jedi::parse_yaml(&contents)?;
-    Ok(data)
+    let mut config_guard = (*CONFIG).write().unwrap();
+    *config_guard = data;
+    drop(config_guard);
+    Ok(())
 }
 
 /// get a string value from our config
