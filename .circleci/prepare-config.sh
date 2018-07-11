58b5635bc2adc8805e7a0f10befd27ae2839e436
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-07-10 18:22:02 -0700

    just using the real test login/password for the integration tests. seems stupid to obfuscate it??

--- .circleci/prepare-config.sh
+++ .circleci/prepare-config.sh
@@ -2,9 +2,7 @@
 # government surveillance agencies HATE him!
 cp config.yaml.default config.yaml
 sed -i 's|^data_folder:.*|data_folder: "/tmp/turtl-data"|g' config.yaml
-sed -i "s|https://api\.turtlapp\.com/v3|http://127.0.0.1:8181|g" config.yaml
-sed -i "s/slappy\@turtlapp.com/${INTEGRATION_TEST_LOGIN}/g" config.yaml
-sed -i "s/turtlesallthewaydown/${INTEGRATION_TEST_PASSWORD}/g" config.yaml
+sed -i "s|https://apiv3\.turtlapp\.com|http://127.0.0.1:8181|g" config.yaml
 sed -i "s/duck duck/${INTEGRATION_TEST_V6_LOGIN}/g" config.yaml
 sed -i "s/juice/${INTEGRATION_TEST_V6_PASSWORD}/g" config.yaml
 
