3efcd5219e11851e4171986d7ed81c2f1344794a
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-10-30 20:25:42 -0700

    setting very very low (energy) poll timeout for stupid fucking ci tests

--- .circleci/prepare-config.sh
+++ .circleci/prepare-config.sh
@@ -2,6 +2,7 @@
 # government surveillance agencies HATE him!
 cp config.yaml.default config.yaml
 sed -i 's|^data_folder:.*|data_folder: "/tmp/turtl-data"|g' config.yaml
+sed -i 's|poll_timeout:.*|poll_timeout: 10|g' config.yaml
 sed -i "s|https://apiv3\.turtlapp\.com|http://127.0.0.1:8181|g" config.yaml
 sed -i "s/duck duck/${INTEGRATION_TEST_V6_LOGIN}/g" config.yaml
 sed -i "s/juice/${INTEGRATION_TEST_V6_PASSWORD}/g" config.yaml
