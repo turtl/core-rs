f1713adb4015c88be1e67320d297064b14893bc6
Andrew Danger Lyon <orthecreedence@gmail.com>
2019-04-10 21:51:35 -0700

    fix set_api_endpoint test

--- integration-tests/tests/set_api_endpoint.rs
+++ integration-tests/tests/set_api_endpoint.rs
@@ -7,7 +7,7 @@ mod tests {
     #[test]
     fn set_api_endpoint() {
         let handle = init();
-        dispatch_ass(json!(["app:api:set-endpoint", "http://api.turtl.dev:8181"]));
+        dispatch_ass(json!(["app:api:set-config", {"endpoint": "http://api.turtl.dev:8181"}]));
         end(handle);
     }
 }
