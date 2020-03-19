008100ea8245aad817dea8fc679ef8a8d59c6248
Andrew Danger Lyon <orthecreedence@gmail.com>
2020-03-18 23:46:55 -0700

    adding emscripten build script

---
+++ scripts/build-emscripten.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+make \
+	FEATURES="sqlite-static wasm" \
+	CARGO_BUILD_ARGS="--target wasm32-unknown-emscripten" \
+	PKG_CONFIG_ALLOW_CROSS=1 \
+	RUSTFLAGS="-L/usr/include/x86_64-linux-gnu -L/usr/include" \
+	release
+
